93156fb40ff0e468d0db90018e31d8bd78036fec
Arkadiusz Rozmus <qrchackofficial@gmail.com>
2019-04-09 08:41:49 +0200

    add cd to install script

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@
 # exit on errors
 set -e
 
+# cd to dotfiles
+HERE=$(cd $(dirname "$BASH_SOURCE"); pwd)
+cd "$HERE"
+
 # change shell to zsh, if exists
 chsh -s $(grep /zsh$ /etc/shells | tail -1)
 
